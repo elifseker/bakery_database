80e038ac6f842f9ff33df33c03010bbe52368fb2
elifseker <sekere@oregonstate.edu>
2022-03-03 01:07:28 -0800

    all select queries done

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 40404);
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
     res.render('home');
 });
 
-app.get('/employees', function (req, res) {
+app.get('/employees', function (req, res, next) {
     let selectString = "SELECT * FROM employees";
     mysql.pool.query(selectString, function (err, rows, fields) {
         if (err) {
@@ -26,23 +26,51 @@ app.get('/employees', function (req, res) {
     })
 });
 
-app.get('/customers', function (req, res) {
-    res.render('customers');
+app.get('/customers', function (req, res, next) {
+    let selectString = "SELECT * FROM customers";
+    mysql.pool.query(selectString, function (err, rows, fields) {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('customers', { "rows": rows });
+    })
 });
 
-app.get('/orders', function (req, res) {
-    res.render('orders');
+app.get('/orders', function (req, res, next) {
+    let selectString = "SELECT * FROM orders";
+    mysql.pool.query(selectString, function (err, rows, fields) {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('orders', { "rows": rows });
+    })
 });
 
-app.get('/products', function (req, res) {
-    res.render('products');
+app.get('/products', function (req, res, next) {
+    let selectString = "SELECT * FROM products";
+    mysql.pool.query(selectString, function (err, rows, fields) {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('products', { "rows": rows });
+    })
 });
 
-app.get('/productcategories', function (req, res) {
-    res.render('productcategories');
+app.get('/productcategories', function (req, res, next) {
+    let selectString = "SELECT * FROM productCategories";
+    mysql.pool.query(selectString, function (err, rows, fields) {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('productcategories', { "rows": rows });
+    })
 });
 
-app.get('/productorders', function (req, res) {
+app.get('/productorders', function (req, res, next) {
     res.render('productorders');
 });
 
